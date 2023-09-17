e7beb59738e58de3f54d8b13f2926967f6ac2a2b
Lucas Pearson <catdevman@gmail.com>
2023-09-16 21:40:48 -0400

    a little stuck need to commit

--- lua/runme.lua
+++ lua/runme.lua
@@ -1,3 +1,5 @@
+local v = vim
+
 ---@type integer win id
 local win
 
@@ -17,8 +19,8 @@ local runme = {}
 ---@field height integer floating window height
 -- default configurations
 local config = {
-  runme_path = vim.fn.exepath("runme"),
-  install_path = vim.env.HOME .. "/.local/bin",
+  runme_path = v.fn.exepath("runme"),
+  install_path = v.env.HOME .. "/.local/bin",
   width = 100,
   height = 100,
 }
@@ -27,7 +29,7 @@ local config = {
 runme.config = config
 
 local function err(msg)
-    vim.notify(msg, vim.log.levels.ERROR, { title = "runme" })
+    v.notify(msg, v.log.levels.ERROR, { title = "runme" })
 end
 
 
@@ -37,13 +39,13 @@ local function release_file_url()
   local version = "1.7.3"
 
   -- check pre-existence of required programs
-  if vim.fn.executable("curl") == 0 or vim.fn.executable("tar") == 0 then
+  if v.fn.executable("curl") == 0 or v.fn.executable("tar") == 0 then
     err("curl and/or tar are required")
     return ""
   end
 
   -- local raw_os = jit.os
-  local raw_os = vim.loop.os_uname().sysname
+  local raw_os = v.loop.os_uname().sysname
   local raw_arch = jit.arch
   local os_patterns = {
     ["Windows"] = "windows",
@@ -71,6 +73,61 @@ local function release_file_url()
   return "https://github.com/stateful/runme/releases/download/v" .. version .. "/" .. filename
 end
 
+local function run(opts)
+  local file
+
+  -- check if runme binary is valid even if filled in config
+  if v.fn.executable(runme.config.runme_path) == 0 then
+    err(
+      string.format(
+        "could not execute runme binary in path=%s . make sure you have the right config",
+        runme.config.runme_path
+      )
+    )
+    return
+  end
+
+  local filename = opts.fargs[1]
+
+  if filename ~= nil and filename ~= "" then
+    -- check file
+    file = opts.fargs[1]
+    if not v.fn.filereadable(file) then
+      err("error on reading file")
+      return
+    end
+
+    local ext = v.fn.fnamemodify(file, ":e")
+    if not is_md_ext(ext) then
+      err("preview only works on markdown files")
+      return
+    end
+  else
+    if not is_md_ft() then
+      err("preview only works on markdown files")
+      return
+    end
+
+    file = tmp_file()
+    if file == nil then
+      err("error on preview for current buffer")
+      return
+    end
+    tmpfile = file
+  end
+
+  stop_job()
+
+  local cmd_args = { runme.config.runme_path, "-s", runme.config.style }
+
+  if runme.config.pager then
+    table.insert(cmd_args, "-p")
+  end
+
+  table.insert(cmd_args, file)
+  open_window(cmd_args)
+end
+
 local function install_runme(opts)
   local release_url = release_file_url()
   if release_url == "" then
@@ -82,16 +139,16 @@ local function install_runme(opts)
   local extract_command = { "tar", "-zxf", "runme.tar.gz", "-C", install_path }
   local output_filename = "runme.tar.gz"
   ---@diagnostic disable-next-line: missing-parameter
-  local binary_path = vim.fn.expand(table.concat({ install_path, "runme" }, "/"))
+  local binary_path = v.fn.expand(table.concat({ install_path, "runme" }, "/"))
 
   -- check for existing files / folders
-  if vim.fn.isdirectory(install_path) == 0 then
-    vim.loop.fs_mkdir(runme.config.install_path, tonumber("777", 8))
+  if v.fn.isdirectory(install_path) == 0 then
+    v.loop.fs_mkdir(runme.config.install_path, tonumber("777", 8))
   end
 
   ---@diagnostic disable-next-line: missing-parameter
-  if vim.fn.filereadable(binary_path) == 1 then
-    local success = vim.loop.fs_unlink(binary_path)
+  if v.fn.filereadable(binary_path) == 1 then
+    local success = v.loop.fs_unlink(binary_path)
     if not success then
       err("runme binary could not be removed!")
       return
@@ -100,15 +157,15 @@ local function install_runme(opts)
 
   -- download and install the runme binary
   local callbacks = {
-    on_sterr = vim.schedule_wrap(function(_, data, _)
+    on_sterr = v.schedule_wrap(function(_, data, _)
       local out = table.concat(data, "\n")
       err(out)
     end),
-    on_exit = vim.schedule_wrap(function()
-      vim.fn.system(extract_command)
+    on_exit = v.schedule_wrap(function()
+      v.fn.system(extract_command)
       -- remove the archive after completion
-      if vim.fn.filereadable(output_filename) == 1 then
-        local success = vim.loop.fs_unlink(output_filename)
+      if v.fn.filereadable(output_filename) == 1 then
+        local success = v.loop.fs_unlink(output_filename)
         if not success then
           err("existing archive could not be removed")
           return
@@ -118,7 +175,7 @@ local function install_runme(opts)
       run(opts)
     end),
   }
-  vim.fn.jobstart(download_command, callbacks)
+  v.fn.jobstart(download_command, callbacks)
 end
 
 ---@return string
@@ -127,28 +184,28 @@ local function get_executable()
     return runme.config.runme_path
   end
 
-  return vim.fn.exepath("runme")
+  return v.fn.exepath("runme")
 end
 
 local function create_autocmds()
-  vim.api.nvim_create_user_command("Runme", function(opts)
+  v.api.nv_create_user_command("Runme", function(opts)
     runme.execute(opts)
   end, { complete = "file", nargs = "?", bang = true })
 end
 
 ---@param params Config? custom config
 runme.setup = function(params)
-  runme.config = vim.tbl_extend("force", {}, runme.config, params or {})
+  runme.config = v.tbl_extend("force", {}, runme.config, params or {})
   create_autocmds()
 end
 
 runme.execute = function(opts)
-  if vim.version().minor < 8 then
-    vim.notify_once("runme.nvim: you must use neovim 0.8 or higher", vim.log.levels.ERROR)
+  if v.version().minor < 8 then
+    v.notify_once("runme.nv: you must use neov 0.8 or higher", v.log.levels.ERROR)
     return
   end
 
-  local current_win = vim.fn.win_getid()
+  local current_win = v.fn.win_getid()
   if current_win == win then
     if opts.bang then
       close_window()
